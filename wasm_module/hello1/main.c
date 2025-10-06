526b3679a44a3342c65533190934467abd5bbe8a
Shohei Kakei <shok1122@zohomail.com>
2025-10-07 01:50:16 +0900

    Implemented hello1 and hello2

--- wasm_module/hello1/main.c
+++ wasm_module/hello1/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("hello\n");
+    printf("hello1\n");
     return 0;
 }
---
+++ wasm_module/hello2/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("hello2\n");
+    return 0;
+}
